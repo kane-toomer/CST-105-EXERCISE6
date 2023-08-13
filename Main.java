aebc4ecb651b0c5caee81276e5761dacb8d4e317
kane-toomer <kanetoomer@yahoo.com>
2023-08-13 13:42:20 -0700

    minor updates

--- Main.java
+++ Main.java
@@ -5,56 +5,56 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
 
-        int m = 4; // Number of rows
-        int n = 8; // Number of columns
+        int kt_m = 4; // Number of rows
+        int kt_n = 8; // Number of columns
 
-        char[][] array = new char[m][n];
+        char[][] kt_array = new char[kt_m][kt_n];
 
         // Initialize the array with '*'
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                array[i][j] = '*';
+        for (int kt_i = 0; kt_i < kt_m; kt_i++) {
+            for (int kt_j = 0; kt_j < kt_n; kt_j++) {
+                kt_array[kt_i][kt_j] = '*';
             }
         }
 
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("input.in"));
-            String inputText = reader.readLine();
-            reader.close();
+            BufferedReader kt_reader = new BufferedReader(new FileReader("input.in"));
+            String kt_inputText = kt_reader.readLine();
+            kt_reader.close();
 
-            int index = 0;
-            for (int i = 0; i < m; i++) {
-                if (i % 2 == 0) { // Even numbered rows
-                    for (int j = 0; j < n && index < inputText.length(); j++) {
-                        if (inputText.charAt(index) != ' ') {
-                            array[i][j] = inputText.charAt(index++);
+            int kt_index = 0;
+            for (int kt_i = 0; kt_i < kt_m; kt_i++) {
+                if (kt_i % 2 == 0) { // Even numbered rows
+                    for (int kt_j = 0; kt_j < kt_n && kt_index < kt_inputText.length(); kt_j++) {
+                        if (kt_inputText.charAt(kt_index) != ' ') {
+                            kt_array[kt_i][kt_j] = kt_inputText.charAt(kt_index++);
                         } else {
-                            j--;
-                            index++;
+                            kt_j--;
+                            kt_index++;
                         }
                     }
                 } else { // Odd numbered rows
-                    for (int j = n - 1; j >= 0 && index < inputText.length(); j--) {
-                        if (inputText.charAt(index) != ' ') {
-                            array[i][j] = inputText.charAt(index++);
+                    for (int kt_j = kt_n - 1; kt_j >= 0 && kt_index < kt_inputText.length(); kt_j--) {
+                        if (kt_inputText.charAt(kt_index) != ' ') {
+                            kt_array[kt_i][kt_j] = kt_inputText.charAt(kt_index++);
                         } else {
-                            j++;
-                            index++;
+                            kt_j++;
+                            kt_index++;
                         }
                     }
                 }
             }
 
-            StringBuilder outputText = new StringBuilder();
-            for (int j = 0; j < n; j++) {
-                for (int i = 0; i < m; i++) {
-                    if (array[i][j] != '*') {
-                        outputText.append(array[i][j]);
+            StringBuilder kt_outputText = new StringBuilder();
+            for (int kt_j = 0; kt_j < kt_n; kt_j++) {
+                for (int kt_i = 0; kt_i < kt_m; kt_i++) {
+                    if (kt_array[kt_i][kt_j] != '*') {
+                        kt_outputText.append(kt_array[kt_i][kt_j]);
                     }
                 }
             }
 
-            System.out.println("Output string: \"" + outputText.toString() + "\"");
+            System.out.println("Output string: \"" + kt_outputText.toString() + "\"");
         } catch (IOException e) {
             e.printStackTrace();
         }
